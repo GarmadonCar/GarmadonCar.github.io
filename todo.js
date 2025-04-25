888c01008b880f5ba703b90addb81dc622af0aa9
LDengah <s1312016@u-aizu.ac.jp>
2025-04-25 22:05:48 +0900

    Unfinished Todo functionality

--- todo.js
+++ todo.js
@@ -1,199 +1,98 @@
-var saved = localStorage.getItem('items');
-
-if (saved) { document.querySelector("#items-list").innerHTML = saved; }
-
-const createNewItem = function (newTodoContent) {
-    const nodeToclone = document.querySelector("#item");
-    const clone = nodeToclone.cloneNode(true);
-    clone.style.display = "flex";
-    let content = clone.childNodes[1];
-    content = content.childNodes[3];
-    content.innerHTML = newTodoContent;
-    clone.classList.add("cloned-item");
-    const itemsList = document.querySelector("#items-list");
-    itemsList.prepend(clone);
-};
-
-const form = document.querySelector("form");
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const newTodoContent = form.elements['new-todo'];
-    if (newTodoContent.value != '') {
-        createNewItem(newTodoContent.value);
-        updateItems();
-        newTodoContent.value = '';
-        saveItems();
-    }
-});
-
-const updateItems = () => {
-    btnCheckBoxs = document.querySelectorAll("#add-list-checkbox");
-    btnCheckBoxs.forEach(btn => {
-        btn.addEventListener('click', function () {
-            btn.classList.add("check-box-clicked");
-            btn.childNodes[1].classList.remove("hidden");
-            btn.disabled = true;
-            btn.classList.remove("add-list-checkbox-light");
-            btn.classList.remove("add-list-checkbox-dark");
-            content = btn.nextElementSibling;
-            content.classList.add("completed");
-            /// add items to  item-list
-            parent = btn.parentElement.parentElement;
-            parent.classList.add("completed");
-            console.log(parent);
-            updateItems();
-            saveItems();
-        })
-    });
-    crossButtons = document.querySelectorAll(".cross-btn");
-    crossButtons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            parentElement = btn.parentElement;
-            parentElement.remove();
-            /// save items list localy
-            saveItems();
-        })
-    });
-    /// save items list localy
-    saveItems();
-};
+// Save list
 const saveItems = function () {
     localStorage.setItem('items', document.querySelector("#items-list").innerHTML);
 }
-updateItems();
 
-/// action buttons
-let itemsList = localStorage.getItem('items');
+var consumeBtn = document.getElementsByClassName("consume");
+var i;
 
-const allBtn = document.querySelectorAll('#all');
-allBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        itemslistParent = document.querySelector("#items-list");
-        itemslistParent.innerHTML = '';
-        itemsList = localStorage.getItem('items');
-        if (itemsList) {
-            itemslistParent.innerHTML = itemsList;
-            updateItems();
-        }
-        btn.classList.add('selected');
-        let activeBtn = document.querySelectorAll('#active');
-        activeBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-        let completedBtn = document.querySelectorAll('#completed');
-        completedBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-    })
-});
+function addConsumeBtn(element) {
+  var consume = document.createElement("SPAN");
+  var txt = document.createTextNode("Consume");
+  consume.className = "consume";
+  consume.appendChild(txt);
+  element.appendChild(consume);
 
-const activeBtn = document.querySelectorAll('#active');
-activeBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
+  for (i = 0; i < consumeBtn.length; i++) {
+    consume[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
 
-        itemslistParent = document.querySelector("#items-list");
-        itemslistParent.innerHTML = '';
-        itemsList = localStorage.getItem('items');
-        if (itemsList) {
-            itemslistParent.innerHTML = itemsList;
-            updateItems();
-        }
-        itemslistParent = document.querySelector("#items-list");
-        itemsList = document.querySelectorAll(".cloned-item");
-
-        itemsList.forEach(item => {
-            completed = false
-            classList = item.classList;
-            classList.forEach(cl => {
-                if (cl === 'completed') {
-                    complited = true;
-                }
-            });
-            if (completed)
-                item.style.display = 'none';
-            else {
-                item.style.disabled = 'block';
-            }
-        });
-
-        btn.classList.add('selected');
-        let allBtn = document.querySelectorAll('#all');
-        allBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-        let completedBtn = document.querySelectorAll('#completed');
-        completedBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-    })
-})
-
-const completedBtn = document.querySelectorAll('#completed');
-completedBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        itemslistParent = document.querySelector("#items-list");
-        itemslistParent.innerHTML = '';
-        itemsList = localStorage.getItem('items');
-        if (itemsList) {
-            itemslistParent.innerHTML = itemsList;
-            updateItems();
-        }
-        itemslistParent = document.querySelector("#items-list");
-        itemsList = document.querySelectorAll(".cloned-item");
-
-        itemsList.forEach(item => {
-            completed = false
-            classList = item.classList;
-            classList.forEach(cl => {
-                if (cl === 'completed') {
-                    completed = true;
-                }
-            });
-            if (!completed)
-                item.style.display = 'none';
-            else {
-                item.style.disabled = 'block';
-            }
-        });
-
-        btn.classList.add('selected');
-        let allBtn = document.querySelectorAll('#all');
-        allBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-        let activeBtn = document.querySelectorAll('#active');
-        activeBtn.forEach(btn => {
-            btn.classList.remove('selected');
-        });
-    })
-})
-
-// clear completed todo
-
-const clearCompletedBtn = document.querySelector('#clear-completed');
-clearCompletedBtn.addEventListener('click', () => {
-    itemslistParent = document.querySelector("#items-list");
-    itemslistParent.innerHTML = '';
-    itemsList = localStorage.getItem('items');
-    if (itemsList) {
-        itemslistParent.innerHTML = itemsList;
-        updateItems();
+      // Animation
     }
-    itemslistParent = document.querySelector("#items-list");
-    itemsList = document.querySelectorAll(".cloned-item");
-    itemsList.forEach(item => {
-        completed = false
-        classList = item.classList;
-        classList.forEach(cl => {
-            if (cl === 'completed') {
-                complited = true;
-            }
-        });
-        if (completed) {
-            //delete completed todo
-            item.remove();
-            updateItems();
-            saveItems();
-        }
-    });
-})
+  }
+}
+
+var yellBtn = document.getElementsByClassName("yell");
+var i;
+
+function addYellBtn(element) {
+  var yell = document.createElement("SPAN");
+  var txt = document.createTextNode("Yell");
+  yell.className = "yell";
+  yell.appendChild(txt);
+  li.appendChild(yell);
+
+  for (i = 0; i < yellBtn.length; i++) {
+    consume[i].onclick = function(){
+      var div = this.parentElement;
+      // Animation
+
+      var inputValue = document.getElementById("input").value;
+      div.innerHTML = inputValue;
+      
+    }
+  }
+}
+
+// Retrieve task list
+function getTask() {
+  var tasklist = localStorage.getItem('items');
+  document.getElementById("p1").innerHTML = tasklist;
+}
+
+// Add new task to list
+function setTask() {
+  var inputId = document.getElementById("myInput").value;
+  var task = localStorage.getItem("inputId");
+
+  var li = document.createElement("li");
+  var t = document.createTextNode(task);
+
+  li.appendChild(t);
+  document.getElementById("todoList").appendChild(li);
+
+  // Add consume button
+  addConsumeBtn(li);
+
+  // Add yell button
+  addYellBtn(li);
+
+  // Make consume button delete Item
+
+
+
+
+
+  saveItems();
+}
+
+// Create a new ID for menu task
+function newId() {
+  var inputValue = document.getElementById("input").value;
+  var id = Math.floor((Math.random() * 999) + 1);
+
+  localStorage.setItem(id, inputValue);
+
+  document.getElementById("input").value = "";
+}
+
+// Display inputs on tablet
+function inputId(number) {
+  display = document.getElementById("input");
+  display += number;
+
+  if (text.length == 3) {
+
+  }
+}
